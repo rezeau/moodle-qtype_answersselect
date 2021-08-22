b4294a4ba511ce14548244bea99c70f9632bad32
rezeau <joseph@rezeau.org>
2021-08-22 22:48:10 +0200

    Updated version number

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'qtype_answersselect';
-$plugin->version   = 2021082000;
+$plugin->version   = 2021082200;
 $plugin->requires  = 2020061500;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.3';
+$plugin->release   = '1.4';
 $plugin->dependencies = array(
     'qtype_multichoice' => 2020061500,
 );
