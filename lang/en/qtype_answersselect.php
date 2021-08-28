a1575244cfdc7790fcb6d1bdda2fe0334a5e148e
rezeau <joseph@rezeau.org>
2021-08-28 23:47:52 +0200

    Tidied up code
    Signed-off-by: rezeau <joseph@rezeau.org>

--- lang/en/qtype_answersselect.php
+++ lang/en/qtype_answersselect.php
@@ -18,7 +18,7 @@
  * Random select answers question type language strings.
  *
  * @package qtype_answersselect
- * 
+ *
  * @copyright 2021 Joseph Rézeau <joseph@rezeau.org>
  * @copyright based on work by 2008 The Open University
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
